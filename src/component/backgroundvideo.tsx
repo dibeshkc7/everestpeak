b4920a111b6526ba44ebfb2b8bf5337288d8cac3
dibeshkc7 <dibeshkc7.com>
2025-04-10 12:38:16 +0545

    slogan changed

--- src/component/backgroundvideo.tsx
+++ src/component/backgroundvideo.tsx
@@ -17,14 +17,15 @@ const BackgroundVideo = () => {
       {/* Overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white px-8">
         <h1 className="text-[55px] font-bold leading-[1.2] max-w-4xl font-poppins">
-          100X Faster Security Reviews <br /> Powered by AI, Verified by Expert
-          Humans
+          AI at Altitude, Fueled by Nature, <br /> Driven by Performance
         </h1>
         {/* Extra Long Line Below Heading */}
         <div className="w-full max-w-4xl h-[2px] bg-white mt-4"></div>
         <p className="mt-6 text-[20px] max-w-3xl font-poppins">
-          The all-in-one, always-on solution for navigating security assessments
-          with unmatched speed and precision.
+          Harnessing Nepal’s cool heights and clean energy, Everest Peak
+          Intelligent Computing delivers cost-effective, eco-friendly HPC
+          solutions tailored for the future of AI and machine learning
+          innovation.
         </p>
         <div className="mt-8 flex space-x-6">
           <button className="px-8 py-3 text-[18px] font-medium bg-white text-black rounded-md border border-gray-300 hover:bg-gray-100 shadow-md font-poppins">
