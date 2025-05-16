128f6e02d468d5dbcebafaa78c5988e6d2061214
dibeshkc7 <dibeshkc7.com>
2025-05-16 13:43:02 +0545

    remove header and footer from login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Introduction from "@/component/introduction";
 import Purpose from "@/component/purpose";
 import Services from "@/component/services";
 import TargetAudience from "@/component/targetaudience";
-import Image from "next/image";
 import Slider from "@/component/slider";
 import BackgroundVideo from "@/component/backgroundvideo";
 import TrustedBy from "@/component/trustedBy";
